d88c37dc62effd91d125feb12ac1d6806d0126bf
Jonathan Liu <jonliu168@gmail.com>
2026-01-08 16:38:54 -0800

    some small thoughts (anything but a meaningful update)

--- src/js/helper.js
+++ src/js/helper.js
@@ -4,3 +4,9 @@ function convertToCSSCoordinate(x,y,)
 {
 
 }
+
+//maybe ill leave code comments actually maybe not because it is just going to be an easy excuse for having un
+//meaningful commits
+//but if i were to do this i would have the date 1/8/2026 of writing it as scatterd thoughts across the codebase
+//that will probably get annoying to remove 
+//but this is a lot harder than i thoguht itd be when life gets so full
