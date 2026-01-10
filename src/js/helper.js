b692834a451c0412a062e76acdcf1981ea52d270
Jonathan Liu <115603886+spooketti@users.noreply.github.com>
2026-01-09 19:31:52 -0800

    not looking too good for me

--- src/js/helper.js
+++ src/js/helper.js
@@ -10,3 +10,5 @@ function convertToCSSCoordinate(x,y,)
 //but if i were to do this i would have the date 1/8/2026 of writing it as scatterd thoughts across the codebase
 //that will probably get annoying to remove 
 //but this is a lot harder than i thoguht itd be when life gets so full
+
+//can't break the streak ahh commit //(cooked repo)
