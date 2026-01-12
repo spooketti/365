254c869b14a8f0e77dd6e782dbbed7a9d06ddab3
Jonathan Liu <jonliu168@gmail.com>
2026-01-11 22:58:17 -0800

    i dont want to deal with this rn but using a text area as input and then make it inviisbleish idk im tired

--- js/hack.js
+++ js/hack.js
@@ -1,8 +1,15 @@
 let testbox = document.getElementById("TEST")
 
-document.addEventListener("click",function(e)
+document.addEventListener("dblclick",function(e)
 {
     testbox.querySelector(".faceContent").style.display = "none"
-    testbox.querySelector(".codeContent").innerText = testbox.querySelector(".faceContent").outerHTML
+    testbox.querySelector(".codeContent").querySelector("code").innerText = testbox.querySelector(".faceContent").outerHTML
     hljs.highlightAll()
+})
+
+document.addEventListener("input",function(e){
+    try{
+    delete e.target.dataset.highlighted
+    }
+    catch{}
 })
