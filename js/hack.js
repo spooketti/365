27430796ab7223ae4ef2e47701b833cf2c2ac3f8
Jonathan Liu <jonliu168@gmail.com>
2026-01-12 15:33:00 -0800

    to do: fix the code input and ifnd a way to make it so you can only edit specific fields of the code so this isn't just glorified inspect

--- js/hack.js
+++ js/hack.js
@@ -3,13 +3,18 @@ let testbox = document.getElementById("TEST")
 document.addEventListener("dblclick",function(e)
 {
     testbox.querySelector(".faceContent").style.display = "none"
-    testbox.querySelector(".codeContent").querySelector("code").innerText = testbox.querySelector(".faceContent").outerHTML
+    testbox.querySelector(".codeContent").querySelector("code").textContent = testbox.querySelector(".faceContent").outerHTML
+    testbox.querySelector(".codeInput").value = testbox.querySelector(".faceContent").outerHTML
     hljs.highlightAll()
 })
 
 document.addEventListener("input",function(e){
+    let codebox = e.target.parentElement.querySelector(".codeContent").querySelector("code")
     try{
-    delete e.target.dataset.highlighted
+    delete codebox.dataset.highlighted
     }
     catch{}
+    codebox.textContent = testbox.querySelector(".codeInput").value
+    hljs.highlightElement(codebox)
+    console.log(testbox.querySelector(".codeInput").value)
 })
