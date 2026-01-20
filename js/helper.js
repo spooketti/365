20e6dad8d5c10e1cdd095f6ef42416c821bb496e
Jonathan Liu <jonliu168@gmail.com>
2026-01-19 23:27:35 -0800

    getelementatcoordinate does work for the time being, but questions arise with z-indexing

--- js/helper.js
+++ js/helper.js
@@ -5,6 +5,11 @@ function convertToCSSCoordinate(x,y,)
 
 }
 
+function getElementAtCoordinate(x,y)
+{
+    return document.elementsFromPoint(x,y)
+}
+
 //maybe ill leave code comments actually maybe not because it is just going to be an easy excuse for having un
 //meaningful commits
 //but if i were to do this i would have the date 1/8/2026 of writing it as scatterd thoughts across the codebase
